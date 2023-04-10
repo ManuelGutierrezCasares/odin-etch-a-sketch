b69988e8ac5b5775d620ef36f6c5e96f85dad64f
Manuel Gutierrez Casares <91099270+ManuelGutierrezCasares@users.noreply.github.com>
2023-04-10 15:13:11 -0300

    Improve logics and heuristics
    Improved logic to the program.
    Improved heuristics to the UI.
    Testing mobile support.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const container = document.querySelector('.container');
-let gridQuantity = 64;
+let input = document.querySelector('#user-input');
+let gridQuantity = Number(prompt('Enter the number of squares per side: '));
+let instructions = document.querySelector('#instructions');
 
-//console.log(container)
+let square = document.querySelectorAll('.square')
+square.forEach(e=>e.addEventListener('mouseover', paintSquares));
+square.forEach(e=>e.addEventListener('touchmove', paintSquares));
+
+const size = document.querySelector('#size');
+size.addEventListener('click',enhanceSize);
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click',resetBox);
+
+let output = document.querySelector('#output');
+output.textContent=`${gridQuantity}x${gridQuantity}`;
+input.value=`${gridQuantity}`;
+
+input.addEventListener('input',e=>output.textContent=`${e.target.value}x${e.target.value}`);
+enhanceSize();
 
 function createSquares(squareAmount){
     for(let i=1;i<=squareAmount;i++){
@@ -10,42 +27,45 @@ function createSquares(squareAmount){
         container.appendChild(newDiv);
     }
 }
-createSquares(gridQuantity);
+
 
 function removeSquares(){
-    //for(let i=1;i<=quantity;i++){
         square.forEach(e=>container.removeChild(e));
-    //}
 };
 
 
-let square = document.querySelectorAll('.square')
-square.forEach(e=>e.addEventListener('mouseover', paintSquares));
 
 function paintSquares(e){
-    console.log(e.target.style.background);
     e.target.style.background = 'black';
 }
 
-const size = document.querySelector('#size');
-size.addEventListener('click',enhanceSize);
+
 
 function enhanceSize(){
-    removeSquares();
-    gridQuantity = 9;
+    if(document.querySelector('.square')){
+        removeSquares();
+    }
+    instructions.textContent='';
+    gridQuantity = Math.pow(input.value,2);
     createSquares(gridQuantity);
     square = document.querySelectorAll('.square')
-    square.forEach(e=>e.style.flex='1 1 133px');
+    let pixels = getPixels(gridQuantity);
+    square.forEach(e=>e.style.flex=`1 1 ${pixels}px`);
     square.forEach(e=>e.addEventListener('mouseover', paintSquares));
+    square.forEach(e=>e.addEventListener('touchmove', paintSquares));
 }
 
-const reset = document.querySelector('#reset');
-reset.addEventListener('click',resetBox);
+
 function resetBox(){
-    removeSquares();
-    gridQuantity = 64;
-    createSquares(gridQuantity);
-    square = document.querySelectorAll('.square')
-    square.forEach(e=>e.style.flex='1 1 50px');
-    square.forEach(e=>e.addEventListener('mouseover', paintSquares));
+    if(document.querySelector('.square')){
+        removeSquares();
+    }
+    input.value = 1;
+    output.textContent = '';
+    instructions.textContent = `Select Size and Click "Adjust Size" to begin`;
+}
+
+
+function getPixels(squares){
+    return 400/Math.sqrt(squares);
 }
